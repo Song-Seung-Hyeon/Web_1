b0ed93ec510f01bbf5af95f3271b7d8f2fd2c584
Song-Seung-Hyeon <129672124+Song-Seung-Hyeon@users.noreply.github.com>
2023-08-30 22:37:43 +0900

    스타벅스
    스타벅스 연습

---
+++ starbucks/script/script.js
@@ -0,0 +1,38 @@
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
+// 돋보기 아이콘을 클릭 시 인풋에 포커스
+searchEl.addEventListener('click', () => {
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', () => {
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색');
+});
+
+searchInputEl.addEventListener('blur', () => {
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
+});
+
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(() => {
+  console.log(window.scrollY)
+  if (window.scrollY > 500) {
+    // 배지 숨기기
+    // gsap.to(요소, 지속시간, 옵션)
+    gsap.to(badgeEl, 0.6, {
+      opacity: 0,
+      display: 'none'
+    });
+  }else {
+    // 배지 보이기
+    gsap.to(badgeEl, 0.6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+}, 300))
+//  __.throttle(함수, 시간)
